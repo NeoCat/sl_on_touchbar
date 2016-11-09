f3d7a4d07f48d5163ea70b9794faaa64abb7430e
NeoCat <neocat@neocat.jp>
2016-11-10 02:23:35 +0900

    Initial Commit

---
+++ sl_on_touchbar/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  sl_on_touchbar
+//
+//  Created by NeoCat on 2016/11/09.
+//  Copyright © 2016 NeoCat. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+}
---
+++ sl_on_touchbar/SLCanvas.swift
@@ -0,0 +1,35 @@
+//
+//  SLCanvas.swift
+//  sl_on_touchbar
+//
+//  Created by NeoCat on 2016/11/09.
+//  Copyright © 2016 NeoCat. All rights reserved.
+//
+
+import Cocoa
+
+class SLCanvas: NSImageView {
+    var x : CGFloat = 0
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        x = self.superview!.bounds.width - 30
+        
+        self.animates = true
+        self.image = NSImage(named: "sl.gif")!
+
+        let timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: updatePosition)
+        timer.fire()
+    }
+    
+    func updatePosition(timer : Timer) {
+        x -= 5
+        self.frame = CGRect(x: x, y: 0, width: self.image!.size.width/2, height: self.image!.size.height/2)
+        if (x < -self.image!.size.width) {
+            NSApplication.shared().terminate(self)
+        } else {
+            NSApplication.shared().activate(ignoringOtherApps: true)
+        }
+    }
+}
